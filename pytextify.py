7ce32b6bbad987085d5dc3d5bffec4b52279091e
NishantKumarABES <nishant543099@gmail.com>
2024-11-14 10:22:14 +0530

    final changes

--- pytextify.py
+++ pytextify.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
     info_msg = st.info("Processing your video ...")
     transcription = video_to_text("uploaded_file/uploaded_video.mp4")
     info_msg.empty()
-    st.success("Transcription is now available")
+    msg = st.success("Transcription is now available")
     def stream_data():
         for word in transcription.split(" "):
             yield word + " "
@@ -26,28 +26,9 @@ if uploaded_file is not None:
 
     with st.container(border=True):
         st.write_stream(stream_data)
+    msg.empty()
 # Footer or additional info
 st.write("---")
 st.write("PyTextify © 2024. Transcribe your videos.")
-
-ad_code = '''
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-7460908811855235"
-     data-ad-slot="XXXXXXX"
-     data-ad-format="auto">
-</ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-'''
-st.markdown(ad_code, unsafe_allow_html=True)
-
-
-# <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7460908811855235"
-# crossorigin="anonymous"></script>
-
-
 
 #################################################################################
