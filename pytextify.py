bec165eab4dabf531e5b821b6c3d634c742cdc34
NishantKumarABES <nishant543099@gmail.com>
2024-11-15 10:44:45 +0530

    add google analytics tag

--- pytextify.py
+++ pytextify.py
@@ -3,56 +3,51 @@ from video_to_text import video_to_text
 import streamlit.components.v1 as components
 import time 
 
+# Set page configuration
 st.set_page_config(layout="wide")
-G_ID = st.secrets["Google_Analytics_Measuremen_ID"]
 
-custom_head = """
-<!DOCTYPE html>
-<html>
-<head>
+# Inject Google Analytics JavaScript in the <head> section
+components.html(
+    """
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WPTDRJYTTT"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YF606H6E0K"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-""" + f""" gtag('config', {G_ID});
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-YF606H6E0K');
     </script>
-</head>
-<body>
-    <h1>Welcome to My Streamlit App</h1>
-    <button onclick="showAlert()">Click Me</button>
-</body>
-</html>
-"""
+    """,
+    height=0,  # Set height to 0 to avoid visible space
+)
 
-# Use Streamlit's HTML component to render the content
-components.html(custom_head, height=300)
-
+# App content
 st.image(r"images/logo_path.png", width=150)
 st.title("Welcome to PyTextify")
-
 st.subheader("Upload Files for Transcription")
-uploaded_file = st.file_uploader(label = " ", type=["mp4"])
+
+# File uploader for video transcription
+uploaded_file = st.file_uploader(label=" ", type=["mp4"])
 if uploaded_file is not None:
     file_name = uploaded_file.name
     with open("uploaded_file/uploaded_video.mp4", "wb") as f:
         f.write(uploaded_file.getbuffer())
-    st.video("uploaded_file/uploaded_video.mp4")  
+    st.video("uploaded_file/uploaded_video.mp4")
 
     info_msg = st.info("Processing your video ...")
     transcription = video_to_text("uploaded_file/uploaded_video.mp4")
     info_msg.empty()
-    msg = st.success("Transcription is now available")
+    st.success("Transcription is now available")
+
+    # Streaming transcription output
     def stream_data():
         for word in transcription.split(" "):
             yield word + " "
-            time.sleep(0.02)   
+            time.sleep(0.02)
 
-    with st.container(border=True):
+    with st.container():
         st.write_stream(stream_data)
+
 # Footer or additional info
 st.write("---")
 st.write("PyTextify © 2024. Transcribe your videos.")
-
-#################################################################################
